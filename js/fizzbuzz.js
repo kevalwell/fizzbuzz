c8139cc9a8626b5b209ff29e6483df7746733121
bungfoon <bungfoon@gmail.com>
2015-08-31 17:57:40 -0400

    finish basic styling + adds js"

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,3 +1,31 @@
+$(document).ready(function(){
+
+  $("#num_input").on("keypress", function(event){
+    if ( event.which == 13 ) {
+      fizz_buzz();
+    }
+  });
+
+  $("#num_submit").on("click", function(){
+
+    fizz_buzz();
+
+  });
+
+
+function fizz_buzz() {
+
+  var num = $("#num_input").val();
+
+  $('.solution').append('<li id="' + num + '">' + num + '</li>');
+  $("#num_input").val('');
+
+};
+
+});
+
+
+
 for (var i=1; i <= 100; i++) {
     if (i % 15 === 0)
         console.log("fizzbuzz");
