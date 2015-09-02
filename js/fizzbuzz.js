487b922cb24e6352de937ff3190e6aa43dfd4074
Kevin Alwell <kevalwell@gmail.com>
2015-09-02 13:07:12 -0400

    Update fizzbuzz.js

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -28,11 +28,11 @@ function fizz_buzz() {
 
 for (var i=1; i <= 100; i++) {
     if (i % 15 === 0)
-        console.log("fizzbuzz");
+        console.log("Swag");
     else if (i % 5 === 0)
-        console.log("buzz");
+        console.log("Sw");
     else if (i % 3 === 0)
-        console.log("fizz");
+        console.log("Ag");
     else
     console.log(i);
 }
